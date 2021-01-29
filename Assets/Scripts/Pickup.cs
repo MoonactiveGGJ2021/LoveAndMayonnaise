9d81020cfad293774a767339b309895ee6bd8267
Yoav Levi <yoav.le@moonactive.com>
2021-01-29 18:21:51 +0200

    Standard assets package & scrips

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    //[SerializeField] AudioClip;
+    //[SerializeField] int points = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //play animation
+        //play audio 
+        //add point
+        Destroy(gameObject);
+    }
+}
