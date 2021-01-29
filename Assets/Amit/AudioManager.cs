18d30902c1f4d41b25c69d55aa2e3f43da864838
Amit Ilan <amit.il@moonactive.com>
2021-01-29 16:33:03 +0200

    Added some audio files

---
+++ Assets/Amit/AudioManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource MusicLoop;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MusicLoop.
+    }
+}
