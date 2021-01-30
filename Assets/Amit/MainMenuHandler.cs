904a63fead23105c31d6a4b1771963ff3c0eba38
Amit Ilan <amit.il@moonactive.com>
2021-01-30 11:04:48 +0200

    Added main menu script

---
+++ Assets/Amit/MainMenuHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    private const string START_ANIMATOR_TRIGGER = "Start";
+    private static readonly int Start = Animator.StringToHash(START_ANIMATOR_TRIGGER);
+
+    [SerializeField] 
+    private Animator CameraAnimator;
+    
+    private bool canStart;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canStart)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                CameraAnimator.SetTrigger(id: Start);
+            }
+        }
+    }
+}
